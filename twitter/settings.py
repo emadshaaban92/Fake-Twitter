7f7a60e0235dc8e419a99a7e0bb4458475fea549
Emad Shaaban <emad.shaaban92@gmail.com>
2013-07-10 05:10:17 +0200

    update settings

--- twitter/settings.py
+++ twitter/settings.py
@@ -14,15 +14,12 @@ from os import environ
 from urlparse import urlparse
 
 if environ.has_key('DATABASE_URL'):
-    url = urlparse(environ['DATABASE_URL'])
-    DATABASES['default'] = {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': url.path[1:],
-        'USER': url.username,
-        'PASSWORD': url.password,
-        'HOST': url.hostname,
-        'PORT': url.port,
-    }
+    # Parse database configuration from $DATABASE_URL
+    import dj_database_url
+    DATABASES['default'] =  dj_database_url.config()
+
+    # Honor the 'X-Forwarded-Proto' header for request.is_secure()
+    SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 else:
     DATABASES = {
         "default": {
